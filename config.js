2736a754d45059f7afcb6480278a984d3fc9b1ad
gcnaveen <naveengc99@gmail.com>
2026-01-21 12:39:50 +0530

    build

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
-export const API_BASE_URL = "http://localhost:4010/";
-export const BASE_URL = "/";
-export const SOCKET_URL = "http://localhost:4010";
-// export const API_BASE_URL = "https://www.fanroom.xyz/api/";
+// export const API_BASE_URL = "http://localhost:4010/";
 // export const BASE_URL = "/";
-// export const SOCKET_URL = "http://www.fanroom.xyz/api/";
+// export const SOCKET_URL = "http://localhost:4010";
+export const API_BASE_URL = "https://www.fanroom.xyz/api/";
+export const BASE_URL = "/";
+export const SOCKET_URL = "http://www.fanroom.xyz/api/";
 export const BASE_LINK = "/";
 export const SOCKET_PATH = "/";
 export const DEFAULT_IP_ADDRESS = "127.0.0.1";
